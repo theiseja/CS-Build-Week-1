764dcebfbed3136ba169b8b6d5f0bc7efdfcffd5
Jesse Theisen <theiseja@gmail.com>
2020-02-28 23:03:00 -0600

    Saving progress

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -154,3 +154,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import django_heroku
 django_heroku.settings(locals())
+
